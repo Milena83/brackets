ae38e1b2e3b1f06fc07561d523698a264352822d
Милена Фёдорова <milenafedorova@MacBook-Air-Milena.local>
2023-02-20 20:20:14 +0300

    add solution, not mine(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let allBrackets = bracketsConfig.join('').replace(/,/g, '');
+  for (let bracket of str) {
+      let bracketsIndex = allBrackets.indexOf(bracket)
+        if (bracketsIndex % 2 === 0) {    
+            if (bracket === allBrackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+                    stack.pop();
+            } else if (bracket === allBrackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                    stack.push(bracketsIndex)
+            }
+            else{
+                    stack.push(bracketsIndex)
+                }
+            } 
+        else {
+                if (stack.pop() !== bracketsIndex-1){
+                    return false;
+                }
+            }
+            
+        }
+        return stack.length === 0
 }
